35afb6c176b7318538e12819ba685c255dfe15ee
Yawn Sean <1900015431@pku.edu.cn>
2023-11-03 15:28:26 +0800

    Add necessary Factorial class

--- Number_Theory/Generalized_Factorial.py
+++ Number_Theory/Generalized_Factorial.py
@@ -9,6 +9,42 @@ def mod_inv(a, p):
     gcd, x, y = ext_gcd(a, p)
     return x % p
 
+class Factorial:
+    def __init__(self, N, mod, h) -> None:
+        N += 1
+        self.mod = mod
+        self.f = [1 for _ in range(N)]
+        self.g = [1 for _ in range(N)]
+        for i in range(1, N):
+            x = i
+            while x % h == 0: x //= h
+            self.f[i] = self.f[i-1] * x % self.mod
+        self.g[-1] = mod_inv(self.f[-1], self.mod)
+        for i in range(N - 2, -1, -1):
+            x = i + 1
+            while x % h == 0: x //= h
+            self.g[i] = self.g[i+1] * x % self.mod
+
+    def fac(self, n):
+        return self.f[n]
+
+    def fac_inv(self, n):
+        return self.g[n]
+
+    def comb(self, n, m):
+        if n < m or m < 0 or n < 0: return 0
+        return self.f[n] * self.g[m] % self.mod * self.g[n - m] % self.mod
+
+    def perm(self, n, m):
+        if n < m or m < 0 or n < 0: return 0
+        return self.f[n] * self.g[n - m] % self.mod
+
+    def catalan(self, n):
+        return (self.comb(2 * n, n) - self.comb(2 * n, n - 1)) % self.mod
+
+    def inv(self, n):
+        return self.f[n-1] * self.g[n] % self.mod
+
 def count_power(x, p):
     ans = 0
     while x:
